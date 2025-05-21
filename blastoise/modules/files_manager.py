c5602e8edd4a6d67e12094607c9a4fd9637a1c5c
ronnyfph <ronnyfph@gmail.com>
2025-05-21 08:59:47 +0200

    Enhance sequence ID and GFF attribute for better traceability
    Updated sequence IDs in `files_manager.py` to include original data for improved identification. Modified GFF attributes in `csvToGff.py` to replace the generic attribute reference with the `qseqid` field for increased clarity and relevance.

--- blastoise/modules/files_manager.py
+++ blastoise/modules/files_manager.py
@@ -24,8 +24,9 @@ def fasta_creator(data_input, fasta_output_path):
     matrix = []
     for index, (_, sequence) in enumerate(data_input.iterrows()):
         # index += 1 # To start the index in 1
+        original_data = f"{sequence['sseqid']}-{sequence['sstart']}-{sequence['send']}-{sequence['sstrand']}-"
         rec = SeqRecord(Seq(sequence.loc["sseq"]),  # In the 5 position is the seq
-                        id="Seq_" + str(index),
+                        id="Seq_" + str(index) + "_" + original_data,
                         description=""
                         )
         matrix.append(rec)
