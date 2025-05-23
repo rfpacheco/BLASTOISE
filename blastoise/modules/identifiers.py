fcf30e349fee0e960069d95ee5e021c8426c9327
ronnyfph <ronnyfph@gmail.com>
2025-05-23 04:37:12 +0200

    Fix unnecessary comma in print statement
    Removed an unintended trailing comma in a print statement that caused formatting issues. This ensures proper rendering of the message in the console output.

--- blastoise/modules/identifiers.py
+++ blastoise/modules/identifiers.py
@@ -63,7 +63,7 @@ def genome_specific_chromosome_main(data_input, main_folder_path, genome_fasta, 
 
 
     print("")
-    print(f"\t\t2.2. Fasta {extend_number} nt file creation:\n"),
+    print(f"\t\t2.2. Fasta {extend_number} nt file creation:")
     sequences_extended_with_sseq = get_data_sequence(sequences_extended, genome_fasta)
     fasta_creator(sequences_extended_with_sseq, sequences_extended_fasta_path, id_names=data_input)
     toc = time.perf_counter()
