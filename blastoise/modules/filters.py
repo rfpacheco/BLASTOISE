bbf5bf167ee9942045f7ef2f8093f1ed03045530
ronnyfph <ronnyfph@gmail.com>
2025-06-17 13:57:46 +0200

    Refactor `filters.py`: replace `chromosome_filter` with `global_filters_main`, streamline logic for data filtering and processing, update docstrings for clarity, and optimize imports by removing `Bio` and adding `pandas`.

--- blastoise/modules/filters.py
+++ blastoise/modules/filters.py
@@ -1,77 +1,32 @@
 import time
-
-from Bio import SeqIO
+import pandas as pd
 
 from modules.genomic_ranges import get_merge_stranded
 
-# -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
 
-
-def chromosome_filter(path_input, name):
+def global_filters_main(data_input: pd.DataFrame, min_length: int) -> pd.DataFrame:
     """
-    Reads a FASTA file and generates a list of chromosome identifiers in a formatted manner.
+    Processes a given DataFrame by applying filtering and modification operations. It adjusts lengths,
+    filters data by a specified minimum length, removes dashes from coordinates, and optionally processes
+    with the BEDOPS method if data remains after filtering.
 
-    With this filter we obtain the labels/titles for each chromosome of our file, e.g., in **Leishmania** case we'll obtain the labels "LinJ.01", "LinJ.02", etc.
-    This filter reads all the sequences in a FASTA file. Then with a prefix ``name``, it adds a numbering in the format ``.XX``, being X the numbers in order for each sequences it finds.
+    Parameters
+    ----------
+    data_input : pd.DataFrame
+        The input DataFrame containing columns 'send' and 'sstart'.
+    min_length : int 
+        The minimum acceptable length for filtering rows.
 
-    For example, for:
-
-    - ``name = "LinJ"``
-    - A fasta file of 36 sequences:
-
-    The output would be:
-
-    .. code-block:: bash
-
-       ['LinJ.01', 'LinJ.02', 'LinJ.03', 'LinJ.04', 'LinJ.05', 'LinJ.06', 'LinJ.07', 'LinJ.08', 'LinJ.09', 'LinJ.10', 'LinJ.11', 'LinJ.12', 'LinJ.13', 'LinJ.14', 'LinJ.15', 'LinJ.16', 'LinJ.17', 'LinJ.18', 'LinJ.19', 'LinJ.20', 'LinJ.21', 'LinJ.22', 'LinJ.23', 'LinJ.24', 'LinJ.25', 'LinJ.26', 'LinJ.27', 'LinJ.28', 'LinJ.29', 'LinJ.30', 'LinJ.31', 'LinJ.32', 'LinJ.33', 'LinJ.34', 'LinJ.35', 'LinJ.36']
-
-    The objective of this function is to be able to correctly name the files since programming languages do not usually admit a non-string format, numbers that start at 0, in this way we can automate their correct labeling, especially for numbers from 01 to 09 .
-
-    .. attention::
-       This IDs need to be matched **exactly** with the row[1] from the CSV to filter.
-
-    Reads a FASTA file and generates a list of chromosome identifiers in a formatted manner.
-
-    Args:
-        path_input (str): The path to the input FASTA file.
-        name (str): A prefix name to be added to each chromosome identifier.
-
-    Returns:
-        list: A list of formatted chromosome identifiers.
+    Returns
+    -------
+    pd.DataFrame
+        A DataFrame that has been processed and filtered according to the specified rules.
     """
-    max_chr = len(list(SeqIO.parse(path_input, "fasta")))  # It reads the FASTA file and gets the total number of chromosomes.
-    chromosome_number = []
-    main_list = (list(range(1, max_chr + 1)))  # We index correctly with "+ 1" since Python starts everything in 0
-    for number in main_list:
-        number = str(number)
-        if len(number) == 1:
-            chromosome_number.append(name + ".0" + number)  # For it to be the same as the CSV
-        else:
-            chromosome_number.append(name + "." + number)
-
-    return chromosome_number
-
-# -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
-
-def global_filters_main(data_input, min_length):
-    """
-    Applies a series of filters to the input data and then processes the data using the ` bedops_main ` function.
-
-    Parameters:
-    data_input (DataFrame): The input data to be filtered.
-    min_length (int): The minimum length to filter the input data.
-
-    Returns:
-    DataFrame: The processed data after applying filters and `bedops_main` function.
-    """
-
     print("\t\t\t- Setting length:")
     data_input["length"] = abs(data_input["send"] - data_input["sstart"]) + 1
 
     print("\t\t\t- Filtering by length:")
-    data_filtered = data_input[data_input["length"].astype(int) >= min_length]  # Filter by length; here it was a 100 before
+    data_filtered = data_input[data_input["length"].astype(int) >= min_length]  # Filter by length
 
     print("\t\t\t- Removing dashes from coordinates:")
     data_filtered = data_filtered.apply(lambda x: x.replace("-", ""))  # Filter dashes
@@ -85,6 +40,6 @@ def global_filters_main(data_input, min_length):
         toc = time.perf_counter()
         print(f"\t\t\t\t- Execution time: {toc - tic:0.2f} seconds")
     else:
-        pass  # Skip the next part of the code
+        pass
 
     return final_data
