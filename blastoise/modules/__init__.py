f617a524ee9cec7d6c5369913c9ccf5ff5c39282
rfpacheco <ronnyfph@gmail.com>
2025-08-19 12:08:51 +0200

    Remove `files_manager` module and update `__init__.py` to reflect the removal.

--- blastoise/modules/__init__.py
+++ blastoise/modules/__init__.py
@@ -1,7 +1,6 @@
 __all__ = [
     'blaster',
     'aesthetics',
-    'files_manager',
     'seq_extension',
     'filters',
     'genomic_ranges'
@@ -10,7 +9,6 @@ __all__ = [
 # Import all modules using relative imports
 from . import blaster
 from . import aesthetics
-from . import files_manager
 from . import seq_extension
 from . import filters
 from . import genomic_ranges
